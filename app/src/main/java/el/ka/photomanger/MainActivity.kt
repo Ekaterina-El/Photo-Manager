c8ff17e570b458c3d8c0e041621d11fa442a4f04
Ekaterina-El <hackerrussia@yandex.ru>
2022-05-05 15:09:51 +0500

    getting photos

--- app/src/main/java/el/ka/photomanger/MainActivity.kt
+++ app/src/main/java/el/ka/photomanger/MainActivity.kt
@@ -1,11 +1,33 @@
 package el.ka.photomanger
 
-import androidx.appcompat.app.AppCompatActivity
+import android.icu.number.NumberFormatter.with
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.squareup.picasso.Picasso
+import el.ka.photomanger.models.PhotoFile
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var photoManager: PhotoManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        photoManager = PhotoManager(this)
+        val photos = photoManager.getAllPPhotos()
+        testShowPhoto(photos[0])
+        Log.d("PHOTOS", "Size: ${photos.size}")
+    }
+
+    private fun testShowPhoto(photoFile: PhotoFile) {
+        val uri = Uri.fromFile(File(photoFile.path))
+        Picasso.get()
+            .load(uri)
+            .into(imageView)
     }
 }
+
---
+++ app/src/main/java/el/ka/photomanger/models/PhotoFile.kt
@@ -0,0 +1,6 @@
+package el.ka.photomanger.models
+
+data class PhotoFile(
+    val path: String,
+    val category: String
+)
